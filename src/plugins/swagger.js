0687260e43b2848ccf18650fea9e21d84e8287dd
Leandro Andrade <leandro.andrade2588@gmail.com>
2022-10-18 11:22:21 -0300

    chore: update dependencies

--- src/plugins/swagger.js
+++ src/plugins/swagger.js
@@ -1,15 +1,20 @@
 const path = require('path');
-const swagger = require('@fastify/swagger');
 const fp = require('fastify-plugin');
+const swagger = require('@fastify/swagger');
+const fastifySwaggerUi = require('@fastify/swagger-ui');
 
 async function swaggerPlugin(fastify, opts) {
     fastify.register(swagger, {
-        routePrefix: '/docs',
         exposeRoute: true,
         mode: 'static',
         specification: {
             path: path.join(__dirname, '..', 'swagger', 'sample-swagger.yaml'),
         },
+    });
+
+    fastify.register(fastifySwaggerUi, {
+        routePrefix: '/docs',
+        logLevel: 'silent',
     });
 }
 
---
+++ tests/integration/swagger.test.js
@@ -0,0 +1,16 @@
+const t = require('tap');
+const { buildApp } = require('../shared/helper');
+
+const { test } = t;
+
+test('should return sample response', async t => {
+    const fastify = buildApp(t);
+
+    const response = await fastify.inject({
+        method: 'GET',
+        url: '/docs',
+    });
+
+    t.equal(response.statusCode, 302);
+    t.equal(typeof response.payload, 'string');
+});
